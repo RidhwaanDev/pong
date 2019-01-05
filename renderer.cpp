742818379d6c2682f8a0754071f32e066fdf2132
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-01-05 16:13:15 -0500

    init

---
+++ renderer.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  renderer.cpp
+ *
+ *    Description:  render textures
+ *
+ *        Version:  1.0
+ *        Created:  01/03/2019 20:34:08
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
